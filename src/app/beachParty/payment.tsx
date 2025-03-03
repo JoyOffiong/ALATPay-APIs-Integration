c9b119f308dc51af9e7fbdd2a87b850ebb517078
joy offiong <blaqkly@gmail.com>
2025-03-03 15:56:35 +0100

    new updates

--- src/app/beachParty/payment.tsx
+++ src/app/beachParty/payment.tsx
@@ -12,6 +12,7 @@ import img from "../../../src/images/beachParty.png";
 import alatpaylogo from "../../../src/images/alatLogo.png";
 import Image from "next/image";
 import { encode } from "punycode";
+import { register } from "module";
 
 function Customer_Details() {
   const [alatPayInitialized, setAlatPayInitialized] = useState(false);
@@ -48,6 +49,8 @@ function Customer_Details() {
     label: p.label,
   }));
 
+  const { register, handleSubmit } = useForm();
+
 
   const submit = (formData: any) => {
     const config = {
@@ -80,20 +83,20 @@ businessId: "1ada836e-ba62-4146-db8b-08dd4ac0a01c",
       console.error("Error initializing AlatPay:", error);
     }
 
-     fetch("/", {
-       method: "POST",
-       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-       body: new URLSearchParams(formData).toString()
-     })
-
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({ "form-name": "contact", ...formData }).toString(),
+    });
   };
 
-  const { control, handleSubmit, getValues } = useForm({ mode: "onChange" });
+
 
   return (
-    <div className="relative  w-full p-8 md:p-12 space-y-5">
-      <div className="top-4 font-light items-center gap-1 text-xs absolute right-4 flex ">
-        <em >Powered by</em> <Image src={alatpaylogo} width={25} height={25} alt="alatpay logo" />
+    <div className="relative w-full p-8 md:p-12 space-y-5">
+      <div className="top-4 font-light items-center gap-1 text-xs absolute right-4 flex">
+        <em>Powered by</em>
+        <Image src={alatpaylogo} width={25} height={25} alt="alatpay logo" />
       </div>
       <div className="flex gap-3 flex-row items-center">
         <Image src={img} width={24} height={24} alt="beach_party_logo" />
@@ -101,89 +104,65 @@ businessId: "1ada836e-ba62-4146-db8b-08dd4ac0a01c",
           Beach_terhousesport Festival
         </p>
       </div>
-      <div className="space-y-4">
-        <p className="font-semibold text-xl md:text-2xl text-[#292524]">
-          Ticket Payment Form
-        </p>
-        <p className="text-[#57534E] text-sm md:text-base font-normal">
-          Get ready to connect, compete, and have fun in a vibrant, empowering
-          atmosphere!
-        </p>
-      </div>
-      <form name="contact" onSubmit={handleSubmit(submit)} data-netlify= "true">
-        <div className="flex mt-8 flex-col gap-6">
-          {/* Title and name */}
+      <form name="contact" onSubmit={handleSubmit(submit)} data-netlify="true">
+        <div className="flex flex-col gap-6 mt-8">
           <div className="flex flex-col md:flex-row gap-8 w-full">
             <div className="w-full">
-              <label htmlFor="" className="text-[#57534E] font-normal text-xs">First Name</label>
-              <InputBoxComp
-                name="firstName"
-                control={control}
-                type="text"
-                placeholder="First Name"
-              />
-            </div>
+              <label className="text-[#57534E] font-normal text-xs">First Name</label>
+              <div className="border-1 border p-3 rounded-xl ">
+
+              <input {...register("firstName")} type="text" placeholder="First Name" required />
+              </div>
+              </div>
             <div className="w-full">
-              <label htmlFor=""  className="text-[#57534E] font-normal text-xs">Last Name</label>
-              <InputBoxComp
-                name="lastName"
-                control={control}
-                type="text"
-                placeholder="Last Name"
-              />
+              <label className="text-[#57534E] font-normal text-xs">Last Name</label>
+              <div className="border-1 border p-3 rounded-xl ">
+
+              <input {...register("lastName")} type="text" placeholder="Last Name" required />
+           
+              </div>
+              </div>
+          </div>
+          <div className="w-full">
+            <label className="text-[#57534E] font-normal text-xs">Email Address</label>
+            <div className="border-1 border p-3 rounded-xl ">
+
+            <input {...register("email")} type="email" placeholder="Email Address" required />
             </div>
+            </div>
+          <div className="w-full flex space-y-2 flex-col">
+            <label className="text-[#57534E] font-normal text-xs">Phone Number</label>
+            <div className="border-1 border p-3 rounded-xl ">
+            <input  {...register("phone")} type="tel" placeholder="Phone Number" required />
           </div>
 
+            </div>
           <div className="w-full">
-            <label htmlFor=""  className="text-[#57534E] font-normal text-xs">Email Address</label>
-            <InputBoxComp
-              name="email"
-              control={control}
-              type="text"
-              placeholder="Email Address"
-            />
-          </div>
-          <div className="w-full">
-            <label htmlFor=""  className="text-[#57534E] font-normal text-xs">Phone Number</label>
-            <InputBoxComp
-              name="phone"
-              control={control}
-              type="text"
-              placeholder="Phone Number"
-            />
-          </div>
+            <label className="text-[#57534E] font-normal text-xs">Ticket Package</label>
+            <div className="border-1 border p-3 rounded-xl ">
 
+            <select {...register("amount")} required className="w-full">
+              {payments.map((p) => (
+                <option key={p.value} value={p.value}>{p.label}</option>
+              ))}
+            </select>
+            </div>
+            </div>
           <div className="w-full">
-            <label htmlFor=""  className="text-[#57534E] font-normal text-xs">Ticket Package</label>
-            <SelectBoxComp
-              data={formattedPayments}
-              name="amount"
-              placeholder="Select Ticket Package"
-              control={control}
-            />
-          </div>
+            <label className="text-[#57534E] font-normal text-xs">Color</label>
+            <div className="border-1 border p-3 rounded-xl ">
 
-          <div className="w-full">
-          <label htmlFor=""  className="text-[#57534E] font-normal text-xs">Color</label>
-              <SelectBoxComp data={colors} name="colors" placeholder="Select your house" control={control}/>
-          </div>
-
-          <div className="hidden">
-            <label htmlFor="">Currency</label>
-            <input
-              name="currency"
-              type="text"
-              defaultValue="NGN"
-            />
-          </div>
+            <select {...register("colors")} required>
+              {colors.map((c) => (
+                <option key={c.value} value={c.value}>{c.label}</option>
+              ))}
+            </select>
+            </div>
+            </div>
+          <input type="hidden" {...register("currency")} value="NGN" />
         </div>
-       
         <div className="my-16 flex justify-end">
-          <button
-            className="w-[203px] h-[48px] min-w-32 py-2 px-6 text-base font-medium rounded-lg text-white 
-               bg-gradient-to-r from-[#EFAB04] to-[#022876] hover:opacity-90 transition-opacity"
-            type="submit"
-          >
+          <button className="w-[203px] h-[48px] min-w-32 py-2 px-6 text-base font-medium rounded-lg text-white bg-gradient-to-r from-[#EFAB04] to-[#022876] hover:opacity-90 transition-opacity" type="submit">
             Proceed to Payment
           </button>
         </div>
